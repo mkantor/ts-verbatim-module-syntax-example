75404a7c3a01fde0d45e4f811da488c73ef4cbe4
Matt Kantor <the.matt.kantor@gmail.com>
2024-03-28 08:39:34 -0400

    Example of verbatimModuleSyntax with strict mode enabled

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { World } from './lib.js' // this line is an error when `verbatimModuleSyntax` is enabled
+
+const world: World = 'world'
+console.log(`Hello, ${world}!`)
---
+++ src/lib.ts
@@ -0,0 +1,2 @@
+export type World = 'world'
+console.log('if you see this, lib.js was imported at runtime')
